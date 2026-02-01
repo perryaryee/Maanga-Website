9440dfd933604c528eaae2c8a6d8bd6a5088c29d
perryaryee <perryaryeesci@gmail.com>
2026-02-01 04:10:27 +0000

    new__updates__push

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import FadeIn from "@/components/animations/FadeIn";
 import PageHero from "@/components/ui/PageHero";
 import Button from "@/components/ui/Button";
-import image_2 from "../../assets/Crekit_2.jpg";
+import image_2 from "../../assets/local.jpg";
 
 export default function Careers() {
     return (
